cdf4346501748a8d3374877d030d7d0b583aed0d
Enter <Enter>
2025-04-07 22:16:55 +0530

    Api response edit

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,10 +1,14 @@
 class ApiResponse {
-  constructor(stausCode, data, message = "Success") {
-    this.statusCode = this.statusCode;
+  constructor(
+    statusCode,
+    data,
+    message = "Success"
+  ) {
+    this.statusCode = statusCode;
     this.data = data;
     this.message = message;
     this.success = statusCode < 400;
   }
 }
 
-export { ApiResponse };
+export { ApiResponse }; 
