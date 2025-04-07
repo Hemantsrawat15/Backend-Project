242cddfc45a1bea6f7b589f8b3dc10892dc03e28
Enter <Enter>
2025-04-07 22:02:23 +0530

    utils code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// Try catch block
+/*
+const asyncHandler = (fn) => {
+  return async (req,res,next) => {
+    try {
+        await fn(req,res,next);
+        
+    } catch (error) {
+        res.status(error.code || 500).json({
+            sucess: false,
+            message: error.message
+        });
+    }
+  };
+};
+*/
